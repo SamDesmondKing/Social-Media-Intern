266edbafa0b2f7e3111a0467c7a53cd361be35e0
Sam <samuel-king@live.com.au>
2020-01-18 16:33:08 +1100

    Added most of the intern's logic, just need to start linking socials.

--- SocialMediaIntern/Intern.cs
+++ SocialMediaIntern/Intern.cs
@@ -1,22 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Windows.ApplicationModel.Appointments.AppointmentsProvider;
 
 namespace SocialMediaIntern
 {
     public sealed class Intern
     {
 
+        private List<Tweet> _tweetHistory = new List<Tweet>();
+
+        //Google authentication here
+
+        //Twitter auth here
+
         public Intern()
         {
-
-            //Create tweet object which constructs tweet
-            //Check tweet against cache here
-            //Call send method of tweet object on tweet
-
-            Debug.WriteLine("Hello World!");
-
+            while (true)
+            { 
+                try
+                {
+                    this.ComposeTweet();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error: " + e.ToString());
+                    //Could even send a tweet with error message here while using throwaway
+                }
+                Thread.Sleep(120000);
+            }
         }
 
+        public void ComposeTweet()
+        {
+            var uniqueTweet = false;
 
+            //Try to compose and send a unique tweet,
+            //will loop until successful.
+            while (!uniqueTweet)
+            { 
+                //Give google auth here
+                var tweet = new Tweet();
+                uniqueTweet = true;
+
+                //Only checks for duplicate URL.
+                foreach (var oldTweet in this._tweetHistory.Where(oldTweet => oldTweet.GetURL() == tweet.GetURL()))
+                {
+                    uniqueTweet = false;
+                }
+
+                if (!uniqueTweet) continue;
+                this.SendTweet(tweet);
+                this.SaveTweet(tweet);
+            }
+        }
+
+        public void SendTweet(Tweet tweet)
+        {
+            //Tweet that shit
+        }
+
+        public void SaveTweet(Tweet tweet)
+        {
+            this._tweetHistory.Add(tweet);
+
+            if (this._tweetHistory.Count > 10)
+            {
+                this._tweetHistory.RemoveAt(0);
+            }
+        }
     }
 }
--- SocialMediaIntern/Tweet.cs
+++ SocialMediaIntern/Tweet.cs
@@ -1,7 +1,36 @@
-﻿namespace SocialMediaIntern
+﻿using System;
+using System.Security.AccessControl;
+using Windows.ApplicationModel.Store.Preview.InstallControl;
+
+namespace SocialMediaIntern
 {
     public sealed class Tweet
     {
+
+        private string _content;
+        private string _URL;
+        private string _hashtags;
+
+        
+
+        public Tweet()
+        {
+
+            //Get content and URL from google sheets
+            //Get hashtags from google sheets
+
+
+        }
+
+        public string ToString()
+        {
+            return this._content + " " + this._URL + " " + this._hashtags;
+        }
+
+        public string GetURL()
+        {
+            return this._URL;
+        }
 
     }
 }
